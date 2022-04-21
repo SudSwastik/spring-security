7cdc3b15e3eb8966611b62edc4c73254694a391d
SudarshanKumar <sudarshan4309@gmail.com>
2022-04-21 16:33:02 +0530

    In Memory Authentication

---
+++ inmemorysecurity/src/test/java/com/sudarshan/inmemorysecurity/InmemorysecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudarshan.inmemorysecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmemorysecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
